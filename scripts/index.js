1c54dbd9732123017852613edf3a7cb7bb55ab6a
adeola idowu <adeolaidowu27@gmail.com>
2020-05-02 22:50:37 +0100

    write javascript logic for game

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,45 @@
 import questions from "./questions.js";
 
-const randomNum = Math.floor(Math.random() * 6);
-const randomQuestion = questions[randomNum];
+const errorMsg = document.querySelector(".error-msg");
 const questionNumHeading = document.querySelector(".question-number");
 const questionContainer = document.querySelector(".question");
 const olNode = document.querySelector(".options__list");
 const wrongScoresPara = document.querySelector(".scores__wrong");
 const rightScoresPara = document.querySelector(".scores__right");
-let playerAnswer;
-let questionNum = 1;
-let rightScore = 0;
-let wrongScore = 0;
-console.log(randomQuestion);
 const submitBtn = document.querySelector(".submit");
 const startBtn = document.querySelector(".start");
+//const randomQuestions = []
+let playerAnswer = "";
+let questionNum = 1;
+let index = 1;
+let rightScore = 0;
+let wrongScore = 0;
 
+console.log(questions.length);
+//function to randomly select a question
 const pickRandomQuestion = () => {
-  const randomNum = Math.floor(Math.random() * 6);
+  const randomNum = Math.floor(Math.random() * questions.length);
   const randomQuestion = questions[randomNum];
   return randomQuestion;
 };
 
+const getRandomQuestions = (questions) => {
+  const randomQuestions = [];
+  const questionsLength = questions.length;
+  for (let i = 0; i < questionsLength; i++) {
+    const randomNum = Math.floor(Math.random() * questionsLength);
+    if (!randomQuestions.includes(questions[randomNum])) {
+      randomQuestions.push(questions[randomNum]);
+    }
+    if (randomQuestions.length === 5) break;
+  }
+  return randomQuestions;
+};
+const randomQuestions = getRandomQuestions(questions);
+
+// function that populates page with question and other html elements
 const displayRandomQuestionPage = (que) => {
-  questionNumHeading.innerText = `Question ${questionNum}`;
+  questionNumHeading.innerText = `Question ${questionNum} of ${randomQuestions.length}`;
   rightScoresPara.innerText = `Correct: ${rightScore}`;
   wrongScoresPara.innerText = `Wrong: ${wrongScore}`;
   questionContainer.innerText = que.question;
@@ -31,39 +48,98 @@ const displayRandomQuestionPage = (que) => {
     listItem.innerText = option;
     listItem.classList.add("options__item");
     olNode.appendChild(listItem);
-    //console.log(listItem);
   });
 };
-//displayRandomQuestion();
 
-const playGame = () => {
-  const randomQuestion = pickRandomQuestion();
-  displayRandomQuestionPage(randomQuestion);
+//function that plays a round of the game
+const playGame = (questions, index) => {
+  //const randomQuestion = pickRandomQuestion();
+  displayRandomQuestionPage(questions[index]);
+  //if player has not made a selection disable next button
+  playerAnswer === "" && submitBtn.setAttribute("disabled", true);
+
   const listItems = document.querySelectorAll(".options__item");
   listItems.forEach((item) => {
     item.addEventListener("click", () => {
-      playerAnswer = item.innerText;
-      console.log(playerAnswer);
-      playerAnswer && checkGame(playerAnswer, randomQuestion);
+      if (playerAnswer !== "") {
+        errorMsg.innerText =
+          "You can only answer this question once. Don't be a thief!";
+      } else {
+        playerAnswer = item.innerText;
+        console.log();
+        checkGame(playerAnswer, questions[index], item, listItems);
+        submitBtn.removeAttribute("disabled");
+      }
     });
   });
 };
 
-const checkGame = (playerResponse, question) => {
+//function to validate player answer
+const checkGame = (playerResponse, question, listItem, listItems) => {
   if (playerResponse == question.answer) {
     rightScore++;
+    listItem.style.backgroundColor = "mediumseagreen";
+    listItem.style.color = "#f7f7f7";
   } else {
     wrongScore++;
+    const answer = question.answer;
+    listItems.forEach((item) => {
+      if (item.innerText === answer) {
+        item.style = "background-color: mediumseagreen; color: #f7f7f7;";
+      }
+    });
+    listItem.style = "background-color: crimson; color: #f7f7f7;";
+
+    console.log(answer);
+  }
+  if (rightScore + wrongScore === 5) {
+    const resultPara = document.querySelector(".result");
+    rightScore > 2.5
+      ? (resultPara.innerText = `You scored ${rightScore}/5  🎊 😊 🎊. Brilliant attempt!!`)
+      : (resultPara.innerText = `You scored ${rightScore}/5 😢😢. You can do better`);
+    startBtn.innerText = "Play Again";
+    resultPara.classList.remove("hide");
+    endGame();
   }
 };
 
+//function to end game
+const endGame = () => {
+  startBtn.classList.remove("hide");
+  document.querySelector(".page-overlay").style.height = "100vh";
+  startBtn.classList.add("btn");
+};
+
+//set default values
+const setDefault = () => {
+  olNode.innerHTML = "";
+  playerAnswer = "";
+  errorMsg.innerText = "";
+  rightScore = 0;
+  wrongScore = 0;
+  questionNum = 1;
+};
+
+//button to start playing game
 startBtn.addEventListener("click", () => {
-  playGame();
+  setDefault();
+  document.querySelector(".page-overlay").style.height = "0";
   startBtn.classList.add("hide");
+  startBtn.classList.remove("btn");
+  setTimeout(() => {
+    playGame(randomQuestions, 0);
+    submitBtn.classList.remove("hide");
+  }, 1000);
 });
 
+//button to move to next question
 submitBtn.addEventListener("click", () => {
+  //debugger;
   olNode.innerHTML = "";
+  playerAnswer = "";
+  errorMsg.innerText = "";
   questionNum++;
-  playGame();
+  playGame(randomQuestions, index);
+  console.log(index);
+  index++;
 });
--- scripts/questions.js
+++ scripts/questions.js
@@ -30,6 +30,55 @@ const questions = [
     answer: "Bathilda Bagshot",
     options: ["Bathilda Bagshot", "Helga Hufflepuff", "Godric Gryffindor"],
   },
+  {
+    question:
+      "One of the following players has won Fifa Balon d'Or with Multiple teams.",
+    answer: "Cristiano Ronaldo",
+    options: ["Lionel Messi", "Cristiano Ronaldo", "Neymar"],
+  },
+  {
+    question:
+      "Which of these is not a quidditch player position in Harry Potter?",
+    answer: "Defender",
+    options: ["Beater", "Defender", "Seeker"],
+  },
+  {
+    question: "Who else could lift Thor's hammer in Avengers.",
+    answer: "Captain America",
+    options: ["Captain America", "The Hulk", "Iron Man"],
+  },
+  {
+    question: "Which one of the following is a CSS framework.",
+    answer: "Bulma",
+    options: ["Goku", "Contour CSS", "Bulma"],
+  },
+  {
+    question: "The 'S' in 'HTTPS' stands for?.",
+    answer: "Secure",
+    options: ["Safe", "Secure", "Script"],
+  },
+  {
+    question:
+      "Javascript is a multi-threaded language that can be none-blocking.",
+    answer: "False",
+    options: ["False", "True", "Don't know"],
+  },
+  {
+    question: "Tim Berners-Lee is best know as the inventor of what?",
+    answer: "World wide web",
+    options: ["HTML", "Javascript", "World wide web"],
+  },
+  {
+    question: "One of the below named is a co-founder of Nike.",
+    answer: "Phil Knight",
+    options: ["Duncan Unsworth", "Phil Knight", "Olivier Armstrong"],
+  },
+  {
+    question:
+      "What is the name of the beautiful actress that plays Wonder Woman?",
+    answer: "Gal Gadot",
+    options: ["Gal Gadot", "Emilia Clarke", "Jennifer Lawrence"],
+  },
 ];
 
 export default questions;
